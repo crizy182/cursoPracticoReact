0e61d4e6d89122df9af7972a15ae20e682c1c0f1
cristubuntu <cristhiam.tovar@mail.escuelaing.edu.co>
2019-10-04 08:51:54 -0500

    25th class,it has an error

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -7,45 +7,47 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
-    <Categories title='Mi lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories title='Originales de platzi Video'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Footer />
-  </div>
-);
+const API = 'http://localhost:3000/initalState';
+const App = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+
+  console.log(videos);
+
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+
+      {
+        videos.myList.length > 0 && (
+          <Categories title='Mi lista'>
+            <Carousel>
+              <CarouselItem />
+            </Carousel>
+          </Categories>
+        )
+      }
+      <Categories title='Originales de platzi Video'>
+        <Carousel>
+          {
+            videos.trends.map(item => <CarouselItem key={item.id} {...item} />)
+          }
+        </Carousel>
+      </Categories>
+      <Categories title='Tendencias'>
+        <Carousel>
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
