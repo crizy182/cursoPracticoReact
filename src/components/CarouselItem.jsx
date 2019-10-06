74354c812bbc14d8fdbcba01ec79dcea769702ce
cristubuntu <cristhiam.tovar@mail.escuelaing.edu.co>
2019-10-05 20:09:20 -0500

    being updated

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -9,8 +9,8 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     <img className='carousel-item__img' src={cover} alt={title} />
     <div className='carousel-item__details'>
       <div>
-        <img className='carousel-item__details--img' src={playIcon} alt='Play Icon' /> 
-        <img className='carousel-item__details--img' src={plusIcon} alt='Plus Icon' /> 
+        <img className='carousel-item__details--img' src={playIcon} alt='Play Icon' />
+        <img className='carousel-item__details--img' src={plusIcon} alt='Plus Icon' />
       </div>
       <p className='carousel-item__details--title'>{title}</p>
       <p className='carousel-item__details--subtitle'>
